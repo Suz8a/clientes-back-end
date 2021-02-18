405717c67c78367581a8500b6f6625584ac1cc00
suz8a <suzbel97@gmail.com>
2021-02-18 14:33:58 -0700

    uploads file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ProspectsController } from './controllers/prospects.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Prospect, ProspectSchema } from './schemas/prospect.schema';
 import { ProspectsService } from './services/prospects.service';
+import { FilesController } from './controllers/file.controller';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { ProspectsService } from './services/prospects.service';
       { name: Prospect.name, schema: ProspectSchema },
     ]),
   ],
-  controllers: [AppController, ProspectsController],
+  controllers: [AppController, ProspectsController, FilesController],
   providers: [AppService, ProspectsService],
 })
 export class AppModule {}
--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,6 +1,47 @@
 import { Storage, Bucket } from '@google-cloud/storage';
+import {
+  Bind,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { v4 } from 'uuid';
 
 const storage = new Storage({ projectId: 'bamboo-almanac-305200' });
 storage.getServiceAccount({}).then((res) => {
   console.log(res);
 });
+
+const bucket = new Bucket(storage, 'prospects-storage');
+bucket.exists().then((exist) => {
+  if (!exist) {
+    storage.createBucket('prospects-storage');
+  }
+});
+
+@Controller('files')
+export class FilesController {
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  @Bind(UploadedFile())
+  uploadFile(file) {
+    console.log(file);
+    return new Promise((resolve) => {
+      const id = v4();
+      const gFile = bucket.file(id);
+      gFile
+        .createWriteStream({ metadata: { contentType: file.mimetype } })
+        .on('error', function (err) {})
+        .on('finish', function () {
+          gFile.makePublic().then((publicFile) => {
+            resolve({
+              url: `https://storage.googleapis.com/prospects-storage/${id}`,
+            });
+          });
+        })
+        .end(file.buffer);
+    });
+  }
+}
